794da261576fb6e3e82d71e3891d9d94cd907b1f
Zhou Lu <zhou.lu@ibm.com>
2018-10-18 15:45:50 -0400

    Implemented post api for register and login

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongose = require('mongoose');
+const Schema = mongose.Schema;
+
+//Create Schema
+const UserSchema = ({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongose.model('users', UserSchema);
